a632327656267f6ce7a288697a53ecdf7f6b33df
Mahi-shetty <srikantayash549@gmail.com>
2026-02-17 19:12:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -411,8 +411,3 @@ def api_subtitle():
         headers={"Content-Disposition": f'attachment; filename="subtitles_{sub_lang}.srt"'}
     )
 
-
-if __name__ == "__main__":
-    import os
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
